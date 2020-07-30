dcd95f4465e36e1774aaed2e62b4bc407a5d1ca7
Aaditya Nigam <aaditya.nigam@sap.com>
2020-07-30 14:26:30 +0530

    Removing due to long filename issue
    clone fails due to long file names. As zips are also uploaded removing the unziped folders.

--- Recipes/for/Enterprise Integration Patterns/Message Construction/2 Command Message/Command Message - Invoking an OData function import/src/main/resources/script/Log.groovy
+++
@@ -1,9 +0,0 @@
-import com.sap.gateway.ip.core.customdev.util.Message
-
-def Message processData(Message message) {
-    
-    def messageLog = messageLogFactory.getMessageLog(message)
-    messageLog.addAttachmentAsString('Product Draft', message.getBody(String), 'application/xml')
-    
-    return message
-}
--- Recipes/for/Enterprise Integration Patterns/Message Construction/6 Return Address/Return Address - Processing Response/src/main/resources/script/Log.groovy
+++
@@ -1,9 +0,0 @@
-import com.sap.gateway.ip.core.customdev.util.Message
-
-def Message processData(Message message) {
-    
-    def messageLog = messageLogFactory.getMessageLog(message)
-    messageLog.addAttachmentAsString('Response', message.getBody(String), null)
-    
-    return message
-}
